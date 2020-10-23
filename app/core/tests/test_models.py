f93cfb83aa72b20ce9465f1abd6675cd71bd940b
Milon Mahato <milonmahato67@gmail.com>
2020-10-23 16:18:48 +0600

    Added Customer User Model

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,41 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        email = "milon@gmail.com"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = 'test@GMAIL.com'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password="test123"
+        )
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                None,
+                password="test123"
+            )
+
+    def test_create_new_superuser(self):
+        user = get_user_model().objects.create_superuser(
+            email='test@gmail.com',
+            password="test123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_stuff)
