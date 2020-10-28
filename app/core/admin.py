d90bb0ae175ad95870adaf5e2e1c7757b1155c5d
Milon Mahato <milonmahato67@gmail.com>
2020-10-29 01:13:34 +0600

    Added recipe model

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 
-from core.models import User, Tag, Ingredient
+from core.models import User, Tag, Ingredient, Recipe
 
 
 class UserAdmin(BaseUserAdmin):
@@ -30,3 +30,4 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(User, UserAdmin)
 admin.site.register(Tag)
 admin.site.register(Ingredient)
+admin.site.register(Recipe)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 
 def sample_user(email='test@gmail.com', password='testpass'):
@@ -62,17 +62,17 @@ class ModelTests(TestCase):
         )
         self.assertEqual(str(ingredient), ingredient.name)
 
-    # def test_recipe_str(self):
-    #     """Test the recipe string representation"""
-    #     recipe = Recipe.objects.create(
-    #         user=sample_user(),
-    #         title='Steak and mushroom sauce',
-    #         time_minutes=5,
-    #         price=5.00
-    #     )
-    #
-    #     self.assertEqual(str(recipe), recipe.title)
-    #
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
     # @patch('uuid.uuid4')
     # def test_recipe_file_name_uuid(self, mock_uuid):
     #     """Test that image is saved in the correct location"""
