dee92e33fb6c3d344ecec9c9b38e790b74def153
Milon Mahato <milonmahato67@gmail.com>
2020-10-28 23:55:44 +0600

    Added create tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -59,21 +59,24 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
 
-    # def test_create_tag_successful(self):
-    #     """Test creating a new tag"""
-    #     payload = {'name': 'Test tag'}
-    #     self.client.post(TAGS_URL, payload)
-    #
-    #     exists = Tag.objects.filter(user=self.user, name='Test tag').exists()
-    #     self.assertTrue(exists)
-    #
-    # def test_create_tag_invalid(self):
-    #     """Test creating a new tag with invalid payload"""
-    #     payload = {'name': ''}
-    #     res = self.client.post(TAGS_URL, payload)
-    #
-    #     self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-    #
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Test tag'}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name='Test tag'
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
     # def test_retrieve_tags_assigned_to_recipes(self):
     #     """Test filtering tags by those assigned to recipes"""
     #     tag1 = Tag.objects.create(user=self.user, name="Breakfast")
